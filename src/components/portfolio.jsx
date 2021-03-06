9fa620edd0ac256d790c0bccbc418b3e92f934b3
yhoungdev <yhoungdev@gmail.com>
2021-03-06 23:01:57 +0100

    hahah

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,48 +1,154 @@
 import React from 'react';
 import Top from './top'
 import '../App.css'
+import View from './prev';
 const Port=()=>{
     return (
+
         <div>
+         
              <Top home="HOME" portfolio="PORTFOLIO" contact="CONTACT"/>
-            {/* <div className="about_me">
-                <h3 className="do">TECHNOLOGIES AND TOOLS I USE</h3>
-                <span className="empty"></span>
-                <span className="talk">
-                    <p>
-                        this are some of the technologies i use to beautify the web for you ,
-                    </p>
-                </span>
-            </div> */}
+           
+          <div className="wrap-card">
 
-            {/* the technologies part */}
-            
-            {/* <h3 className="do">Languages </h3> */}
-            {/* <span className="empty"></span>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sint quo dolores eligendi ipsam blanditiis iusto sed sapiente quaerat! Dolor illum, impedit minus porro veniam dolores nisi non facilis pariatur?</p>
-
-
-            <div className="projects">
-                <h3 className="do">PORFOLIO</h3>
-                <span className="empty"></span>
-
-                <p>List of my works and project </p>
-                <div className="work">
-
-
-                        <div className="card">
-                            <div className="card-child">
-                               
-                            </div>
-                        </div>
-
+          <div className="card">
+              <div className="title">
+                  <h6 className="project-title">MailDevo</h6>
+              </div>
+                <div className="card-item">
+                    <span className='tell'>
+                        <p>
+                            A simple mailing service , which comes with a simple and well documented API
+                        </p>
+                    </span>
                 </div>
-            
-             
 
+                <div className="view-part">
+                    <View stat="Completed"/>
+                </div>
+            </div>
 
-            </div> */}
-            <center><h4>Cooming soon</h4></center>
+            <div className="card">
+              <div className="title">
+                  <h6 className="project-title">BAALEN</h6>
+              </div>
+                <div className="card-item">
+                    <span className='tell'>
+                        <p>
+                             A web application which aids learning , by giving you some learning resources 
+                        </p>
+                    </span>
+                </div>
+
+                <View stat="Completed"/>
+            </div>
+
+           
+
+            <div className="card">
+              <div className="title">
+                  <h6 className="project-title">NOTFLIX</h6>
+              </div>
+                <div className="card-item">
+                    <span className='tell'>
+                        <p>
+                            NetFlix Desktop version clone 
+                            Desktop ( Desktop view ) version for Netflix website 
+                        </p>
+                
+                    </span>
+                </div>
+
+                <View stat="Completed"/>
+            </div>
+
+            <div className="card">
+              <div className="title">
+                  <h6 className="project-title">O'CART</h6>
+              </div>
+                <div className="card-item">
+                    <span className='tell'>
+                        <p>
+                            Demo of a simple carting app for desktop view ( Desktop version )
+                        </p>
+                    </span>
+                </div>
+                <View cons="under-construction" />
+            </div>
+
+        
+
+          </div>
+
+            {/* other flex part */}
+            <br/>
+
+            <div className="wrap-card" >
+
+            <div className="card">
+              <div className="title">
+                  <h6 className="project-title">GROWYN</h6>
+              </div>
+                <div className="card-item">
+                    <span className='tell'>
+                        <p>
+                            Grow your social media account by getting more followers with just a simple click
+                        </p>
+                    </span>
+                </div>
+
+                <View stat="Completed"/>
+               
+            </div>
+
+            <div className="card">
+              <div className="title">
+                  <h6 className="project-title">COVID-19 TRACKER</h6>
+              </div>
+                <div className="card-item">
+                    <span className='tell'>
+                        <p>
+                            Simple covid-19 tracker for all countries ,
+                            shows the rate and cases of covid 19 in a particular country 
+                        </p>
+                    </span>
+                </div>
+
+                <View stat="Completed"/>
+            </div>
+
+            <div className="card">
+              <div className="title">
+                  <h6 className="project-title">CODE4MOM.js</h6>
+              </div>
+                <div className="card-item">
+                    <span className='tell'>
+                        <p>
+                            A small Javascript Library for DOM manipulation , which has made accessing the Dom easy 
+                        </p>
+                    </span>
+                </div>
+
+                <View open="open source"/>
+            </div>
+
+            <div className="card">
+              <div className="title">
+                  <h6 className="project-title">INSTACLONE</h6>
+              </div>
+                <div className="card-item">
+                    <span className='tell'>
+                        <p>
+                        Instagram profile page clone , just for fun and have the feel of insta 
+                        </p>
+                    </span>
+                </div>
+
+                <View stat="Completed"/>
+            </div>
+
+            </div>
+
         </div>
     )
 }
---
+++ src/components/prev.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {AiFillGithub , AiOutlineFundView , AiOutlineFolderView} from 'react-icons/ai'
+const View= props => {
+    return (
+        <div>
+           <div className="preview">
+               <button className="btn-view">Live Demo</button>
+
+               <div className="line">
+                    <h2 className="git_link"><AiFillGithub/></h2>
+
+                   
+               </div>
+               <div className="stat">
+                 <h6>{props.stat}</h6>
+                 <h6 style={{color:'rgba(156, 156, 9, 0.89)'}}>{props.cons}</h6>
+                 <h6 style={{color:'rgba(18, 108, 224, 0.541)'}}>{props.open}</h6>
+                 
+               </div>
+           </div>
+        </div>
+    )
+}
+export default View ;
